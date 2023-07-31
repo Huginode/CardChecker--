53c39992c21a92fe053948bb6c5378392cf884cd
TheCringiestCoder <73342382+SomebodyTouchMySpaghet@users.noreply.github.com>
2023-08-01 00:08:07 +0200

    Crash: Segementation fault + logical error
    Crash occured during segmentation fault. halfy resolved, still a problem in line 97.
    
    Plus there is a logical fault the variable is way too big after the calculations making it crash if the number is too big (like a true credit card number)

--- creditCardValidator.cpp
+++ creditCardValidator.cpp
@@ -6,16 +6,27 @@ using namespace std;
 
 int main(){
     
-    
+int i = 0;   
 
-int num = 0;
-int i = 0;
+//var for card number
+long num = 0;
+
+//all the variables for Luhn's algorithm
 int Luhn = 0;
 int Luhn1 = 0;
-int sum = 0;
+
+//var created because of a segmentation fault
+int push = 0;
+
+//sum for the last step of the luhn's algorithm
+long sum = 0;
+
+//all the vectors for the luhn algorithm
 vector <int> luhn;
 vector <int> luhn1;
-vector <long> creditCard;
+vector <int> creditCard;
+
+
 
     
     cout << "Please Input your credit card number.\n";
@@ -29,13 +40,15 @@ vector <long> creditCard;
 
     //Luhn's algorith : take every number with an even rank and an odd rank. Multiply them by 2.
     
-    if (creditCard.size() == 14){
+    if (creditCard.size() == 16){
         for (i = 0; i <= creditCard.size(); i = i + 2){
             if(i % 2 == 0){
-                luhn.push_back(creditCard[i*2]);
+                push = creditCard[i]*2;
+                luhn.push_back(push);
             }
             else {
-                luhn1.push_back(creditCard[i*2]);
+                push = creditCard[i]*2;
+                luhn1.push_back(push);
             }
             
         }
@@ -65,10 +78,12 @@ vector <long> creditCard;
     else {
         for (i = 0; i <= creditCard.size(); i = i + 2){
             if(i % 2 == 0){
-                luhn.push_back(creditCard[i*2]);
+                push = creditCard[i]*2;
+                luhn.push_back(push);
             }
             else {
-                luhn1.push_back(creditCard[i*2]);
+                push = creditCard[i]*2;
+                luhn1.push_back(push);
             }
             
         }
@@ -76,7 +91,8 @@ vector <long> creditCard;
         for(i = 0; i <= luhn.size(); i++){
             Luhn = Luhn + luhn[i];
 
-        } 
+        }
+
         for(i = 0; i <= luhn1.size(); i++){
             Luhn1 = Luhn1 + luhn1[i];
 
