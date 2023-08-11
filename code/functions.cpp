14c334f3bbc5767ab9537fc22707d8f09e377684
TheCringiestCoder <73342382+SomebodyTouchMySpaghet@users.noreply.github.com>
2023-08-11 18:41:00 +0200

    made code more readable

--- code/functions.cpp
+++ code/functions.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //Erase function to solve later problems
 
 template<typename T>
-void pop_front(std::vector<T> &v) {
+void pop_front(vector<T> &v) {
     if (v.size() > 0) {
         v.erase(v.begin());
     }
